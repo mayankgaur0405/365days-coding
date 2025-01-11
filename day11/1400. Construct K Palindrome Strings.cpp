c35851b95e73cd51c3d80155721d13a317deaa79
Mayank Gaur <mayankgaur1504@gmail.com>
2025-01-11 23:29:52 +0530

    day11 completed

---
+++ day11/1400. Construct K Palindrome Strings.cpp
@@ -0,0 +1,20 @@
+// class Solution {
+// public:
+//     bool canConstruct(string s, int k) {
+//         if(s.size() < k) return false;
+//         if(s.size() == k) return true;
+//         map<char, int> count;
+        
+//         for(char c : s){
+//             count[c]++;
+//         }
+        
+//         int odd = 0;
+//         for(auto it = count.begin(); it != count.end(); it++){
+//             if(it->second % 2 == 1){
+//                 odd++;
+//             }
+//         }
+//         return odd <= k;
+//     }
+// };
