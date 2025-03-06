ad076fb9f7f2915f4665836065c8b8dbd7d437f2
Mayank Gaur <mayankgaur1504@gmail.com>
2025-03-06 21:40:36 +0530

    day65 completed

---
+++ day65[06-03-2025]/potd.cpp
@@ -0,0 +1,8 @@
+// class Solution {
+//     public:
+//         long long coloredCells(int n) {
+//             long long box = (1LL * (n - 1) * n) / 2;
+//             return 1 + 4 * box;
+//         }
+//     };
+    
